a9480b3e2fa91e9199154f8417fd4128c45cc16b
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-17 20:12:24 +0300

    Taking input from user

--- JString.java
+++ JString.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class JString {
     public static void main(String [] args){
         Scanner sc =new Scanner(System.in);
+        String input= sc.nextLine();
+        int length = sc.nextInt();
+        String smallest ="";
+        String largest="";
     }
 }
