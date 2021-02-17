1d75551d48c3363ff4e08e793c7cb9119f7c2392
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-17 20:24:49 +0300

    adding functionality

--- JString.java
+++ JString.java
@@ -3,9 +3,24 @@ import java.util.Scanner;
 public class JString {
     public static void main(String [] args){
         Scanner sc =new Scanner(System.in);
+        System.out.println("Please enter your sentence");
         String input= sc.nextLine();
         int length = sc.nextInt();
         String smallest ="";
         String largest="";
+        for (int i=0;i<=input.length()-length;i++) {
+            String s1 = input.substring(i, i + length);
+            if (i == 0) {
+                smallest = s1;
+            }
+            if (s1.compareTo(largest) > 0) {
+                largest = s1;
+            } else if (s1.compareTo(smallest) < 0) {
+                smallest = s1;
+            }
+
+        }
+        System.out.println(smallest);
+        System.out.println(largest);
     }
 }
