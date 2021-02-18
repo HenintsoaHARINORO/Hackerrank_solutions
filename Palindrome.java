d7075d0e3b4556f1657f810d45b895616ac8c454
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-18 18:09:54 +0300

    tests

--- Palindrome.java
+++ Palindrome.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
+
 public class Palindrome {
     public static boolean isPalindrome(String a){
         boolean isPal= true;
         for (int i = 0;i<(int)a.length()/2;i++){
             if (a.charAt(i)!=a.charAt(a.length()-1-i)){
              isPal=false;
-             break;;
+             break;
             }
         }
+        return isPal;
     }
     public static void main(String [] args){
-
+        Scanner sc = new Scanner(System.in);
+        String input= sc.next();
+        System.out.println(isPalindrome(input)?"YES":"NO");
     }
 }
