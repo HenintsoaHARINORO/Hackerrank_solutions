89fd3ad5921a08bacda90c2f50870445d41cc73a
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2021-02-22 14:59:04 +0300

    addition big integers

--- BigInteger.java
+++ BigInteger.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class BigInteger {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String num1= sc.nextLine();
+        String num2 = sc.nextLine();
+        BigInteger big1= new BigInteger();
+        BigInteger big2= new BigInteger();
+        System.out.println(big1.add(big2));
+        System.out.println(big1.multiply(big2));
+    }
+
 }
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
  * JAVA STRING
  * PALINDROME:there is a difference between uppercase and lowercase
  * COMPARATOR
+ * BIGINTEGER
